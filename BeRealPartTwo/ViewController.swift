2a0a3c7db1f4ffc713934506f0aeb30b6de9c9c1
vnunez7 <vnunez7@illinois.edu>
2023-04-07 13:59:00 -0500

    Initial Commit

---
+++ BeRealPartTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeRealPartTwo
+//
+//  Created by Victoria Nunez on 4/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
